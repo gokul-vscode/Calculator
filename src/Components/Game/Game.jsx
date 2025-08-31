f9effb586d80f492072feb16986b83d4723a4b2c
gokul-vscode <ramgokul637@gmail.com>
2025-08-31 22:42:21 +0530

    new

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,53 +1,69 @@
-import React, { useState } from 'react'
-import '../Game/Game.css'
-import { evaluate } from 'mathjs';
+import React, { useState } from "react";
+import "../Game/Game.css";
+import { evaluate } from "mathjs";
 function Game() {
-    const [input,setInput] =useState("");
-    const [result,setResult] = useState("");
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-    const handleClick = (value)=>{
-        if(value ==="="){
-            try{
-                const output = evaluate(input)
-                setResult(output.toString());
-            }catch(error){
-                setResult('error');
-            }
-        }
-        else if(value === "c"){
-            setInput("")
-            setResult("")
-        }
-        else if(value=== '⌫'){
-            setInput(input.slice(0,-1))
-        }
-        else{
-            setInput(input+value)
-        }
-    };
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        const output = evaluate(input);
+        setResult(output.toString());
+      } catch (error) {
+        setResult("error");
+      }
+    } else if (value === "c") {
+      setInput("");
+      setResult("");
+    } else if (value === "⌫") {
+      setInput(input.slice(0, -1));
+    } else {
+      setInput(input + value);
+    }
+  };
 
-    const buttons = [
-        '7','8','9','/',
-        '4','5','6','*',
-        '1','2','3','-',
-        '0','.','=','+',
-        'c','⌫',
-    ];
+  const buttons = [
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "=",
+    "+",
+    "c",
+    "⌫",
+  ];
 
-
-    return (
-        <div className='Calculator'>
-            <div className='display'>
-                <input type="text" value={input} readOnly />
-                <div className='result'>{result}</div>
-            </div>
-            <div className='buttons'>
-                {buttons.map((btn,index)=>(
-                    <button key={index} onClick={()=>handleClick(btn)}>{btn}</button>
-                ))}
-            </div>
+  return (
+    <>
+    <div className="head">
+        <h1>CALCULATOR</h1>
+    </div>
+      <div className="Calculator">
+        <div className="display">
+          <input type="text" value={input} readOnly />
+          <div className="result">{result}</div>
         </div>
-    )
+        <div className="buttons">
+          {buttons.map((btn, index) => (
+            <button key={index} onClick={() => handleClick(btn)}>
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Game
+export default Game;
