6f44da7563ab27918bced6f159e8e3f811d064a6
gokul-vscode <ramgokul637@gmail.com>
2025-08-31 22:11:35 +0530

    first commit

---
+++ src/Components/Game/Game.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import '../Game/Game.css'
+import galaxy from '../../assets/galaxy.jpeg'
+import { IoBackspace } from "react-icons/io5";
+
+function Game() {
+    const [input,setInput] =useState("");
+    const [result,setResult] = useState("");
+
+    const handleClick = (value)=>{
+        if(value ==="="){
+            try{
+                setResult(eval(input).toString());
+            }catch(error){
+                setResult('error');
+            }
+        }
+        else if(value === "c"){
+            setInput("")
+            setResult("")
+        }
+        else if(value=== '⌫'){
+            setInput(input.slice(0,-1))
+        }
+        else{
+            setInput(input+value)
+        }
+    };
+
+    const buttons = [
+        '7','8','9','/',
+        '4','5','6','*',
+        '1','2','3','-',
+        '0','.','=','+',
+        'c','⌫',
+    ];
+
+
+    return (
+        <div className='Calculator'>
+            <div className='display'>
+                <input type="text" value={input} readOnly />
+                <div className='result'>{result}</div>
+            </div>
+            <div className='buttons'>
+                {buttons.map((btn,index)=>(
+                    <button key={index} onClick={()=>handleClick(btn)}>{btn}</button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Game
